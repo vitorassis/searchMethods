fd1a6c31f00c35271c3ce243be6dfdc76a12466e
vitor assis <vitor.a.camargo@live.com>
2019-10-06 22:41:55 -0300

    modified:   exaustiva_com_sentinela.cpp 	modified:   metodos_de_busca.cpp 	modified:   metodos_de_busca.exe

--- metodos_de_busca.cpp
+++ metodos_de_busca.cpp
@@ -6,6 +6,36 @@
 
 #include "exaustiva.cpp"
 #include "exaustiva_com_sentinela.cpp"
+#include "sequencial_indexada.cpp"
+
+void dumpIntVector(int vetor[], int size){
+
+	clearScreen();
+	
+	printf("Array => (\n");
+	for(int i=0; i<size; i++){
+		printf("\t[%d] => %d\n", i, vetor[i]);
+	}
+	printf(")");
+	getch();
+	clearScreen();
+	drawCanvas();
+}
+
+void ordenate(int vetor[], int new_vetor[], int size){
+	for (int i=0; i<size; i++)
+		new_vetor[i] = vetor[i];
+	
+	for(int i=1; i<size; i++)
+		for(int j=0; j<size-1; j++)
+			if(new_vetor[i]<new_vetor[j]){
+				new_vetor[i] ^= new_vetor[j];
+				new_vetor[j] ^= new_vetor[i];
+				new_vetor[i] ^= new_vetor[j];
+			}
+		
+		
+}
 
 void inputVector(int vetor[], int &size){
 	clearCanvas();
@@ -30,13 +60,13 @@ void compare(int vector[], int size){
 	clearCanvas();
 	int busca;
 	int coord;
-	int index, steps;
+	int index, steps, ord_vetor[TF];
 	do{
 		clearCanvas();
 		gotoxy(30, 8);printf("Número: ");
 		busca = readIntVariable(38, 8, 50, 8);
 		char options[6][50];
-		strcpy(options[0], "Mudar número");
+		strcpy(options[0], "Mudar Número");
 		strcpy(options[1], "Busca Exaustiva");
 		strcpy(options[2], "Busca Exaustiva Com Sentinela");
 		strcpy(options[3], "Busca Sequencial Indexada");
@@ -54,7 +84,18 @@ void compare(int vector[], int size){
 					getch();	
 					break;	
 				case 12:
-					index = busca_exaustiva_com_sentila_benckmark(vector, busca, steps);
+					index = busca_exaustiva_com_sentila_benckmark(vector, size, busca, steps);
+					
+					gotoxy(10, 20); printf("Índice: %d", index);
+					gotoxy(10, 21); printf("Passos: %d", steps);			
+					getch();	
+					break;
+				case 13:
+					ordenate(vector, ord_vetor, size);
+					
+					index = busca_sequencial_indexada_benchmark(ord_vetor, size, busca, steps);
+					
+				//	dumpIntVector(ord_vetor, size);
 					
 					gotoxy(10, 20); printf("Índice: %d", index);
 					gotoxy(10, 21); printf("Passos: %d", steps);			
@@ -72,16 +113,17 @@ int main(){
 	int vector[TF+1];
 	int size = 0;
 	
-	char opcoes[3][50];
+	char opcoes[4][50];
 	strcpy(opcoes[0], "Cadastrar");
 	strcpy(opcoes[1], "Testar");
-	strcpy(opcoes[2], "Sair");
+	strcpy(opcoes[2], "Ver vetor");
+	strcpy(opcoes[3], "Sair");
 		
 	int coord;
 	
 	do{
 		clearCanvas();
-		coord = showMenu(opcoes,10, 12, 30);
+		coord = showMenu(opcoes,10, 13, 30);
 		switch(coord){
 			case 10:
 				inputVector(vector, size);
@@ -89,8 +131,10 @@ int main(){
 			case 11:
 				compare(vector, size);
 				break;
+			case 12:
+				dumpIntVector(vector, size);
 		}
-	}while(coord != 12);
+	}while(coord != 13);
 	
 	return 0;
 }
